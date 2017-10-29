232cfa98ca9ce80876bd26e4cf4019e4d239ceb8
Kevin Ye <darth.vader8518@hotmail.com>
2017-10-29 05:51:10 -0400

    list successful matches

---
+++ src/success.py
@@ -0,0 +1,12 @@
+import db as db
+import sys
+
+user = sys.stdin.readline().strip()
+d = db.db()
+#list of documents
+ulist = d.find_user_matches(user)
+for u in ulist:
+	if user == u['user_1'] and u['status'] == 'accepted':
+		print(u['user_2'],u['matcher'],u['_id'],u['comment'],sep="\n")
+	elif user == u['user_2'] and u['status'] == 'accepted':
+		print(u['user_1'],u['matcher'],u['_id'],u['comment'],sep="\n")
